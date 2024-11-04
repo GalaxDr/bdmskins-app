d92bea988f8df3cfd1a2d6b104a106f7b4fb6db0
Júlio Becker <juliobeckinha@gmail.com>
2024-11-04 16:41:51 -0300

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { env } from "process";
 
 interface Skin {
   id: number;
